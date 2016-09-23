bd1c800f17e88466fc0bc4df15110c0202705244
Mugsy Carter <mugsycarter@gmail.com>
2016-09-23 14:13:37 -0700

    "lots of stuff changed"

---
+++ scripts/main.js
@@ -0,0 +1,31 @@
+//on page load the elements with the 'invis' attribute are made invisible
+$(document).ready(function(){
+$("div[data-vis='invis']").hide();
+});
+
+// //this is a test function
+// function clicker(){
+//   $('div').on('click', function(){
+//     $(this).fadeOut('slow');
+//   });
+// };
+
+//This function displays the contact links when clicked
+function linkBar(){
+  $("div[data-vis='vis']").on('click', function(){
+    $(this).slideUp('slow');
+    $('.contact').slideDown('slow');
+  });
+};
+
+//This function displays contact button again when clicked
+function linkBarReturn(){
+  $("div[data-vis='invis']").on('click', function(){
+    $(this).slideUp('slow');
+    $('.foot').slideDown('slow');
+  });
+};
+
+//clicker();
+linkBar();
+linkBarReturn();
